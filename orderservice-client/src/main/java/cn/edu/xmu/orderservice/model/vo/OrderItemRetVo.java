84d57990232a4eb0738ee6b9cbc35609dbad20eb
774431978@qq.com <774431978@qq.com>
2020-12-23 23:48:13 +0800

    update dubbo

--- orderservice-client/src/main/java/cn/edu/xmu/orderservice/model/vo/OrderItemRetVo.java
+++ orderservice-client/src/main/java/cn/edu/xmu/orderservice/model/vo/OrderItemRetVo.java
@@ -8,22 +8,14 @@ import lombok.Data;
 
 @Data
 public class OrderItemRetVo {
-    private Long orderId;
-    private String orderSn;
     private Long orderItemId;
-    private Long skuId;
-    private String skuName;
     private Long customerId;
-    private Long shopId;
 
     public OrderItemRetVo(OrderItem orderItem, OrderPo order)
     {
-        this.orderId=orderItem.getOrderId();
-        this.orderSn=order.getOrderSn();
+
         this.orderItemId=orderItem.getId();
-        this.skuId=orderItem.getGoodsSkuId();
-        this.skuName=orderItem.getName();
+
         this.customerId=order.getCustomerId();
-        this.shopId=order.getShopId();
     }
 }
